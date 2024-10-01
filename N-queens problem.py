464f5bf56dbd7106e5a74b805489278398da0a2a
Tooryou2005 <aboubakrtahir44@gmail.com>
2024-10-01 20:08:20 +0100

    N-queens problem SOLVED BY object-oriented programming (OOP)
    after 5 days of getting my brain beaten over and over again by this problem and the countless explanations about backtracking provided by chatgpt , youtube, and some courses of my university i barely finished it , i cant say that i mastered backtracking , i still need more problems that requires brute force to process solutions , but its still stays a step taken ahead .

---
+++ N-queens problem.py
@@ -0,0 +1,41 @@
+class Queens :
+    def __init__ (self, size) :
+        self.size = size 
+        self.board = [[0] * self.size for _ in range(self.size)]
+    
+    def is_valid(self , row , col) :
+        #lets check for  column first :
+        for i in range(self.size) :
+            if self.board[i][col] == 1 :
+                return False 
+        #check for row after :    
+        for j in range(self.size) :
+            if self.board[row][j] == 1 :
+                return False    
+        #top left diagonal check :
+        for i , j in zip(range(row , -1 ,-1) , range(col , -1 , -1)):
+            if self.board[i][j] == 1 :
+                return False    
+        #top right diagonal check : 
+        for i , j in zip(range(row , -1 , -1) , range(col , self.size)) :
+            if self.board[i][j] == 1 :
+                return False
+        return True     
+    
+    def solve (self,row=0) :
+        if row == self.size :
+            for row in self.board :
+                print(row)
+            return True
+        for col in range(self.size) :
+            if self.is_valid (row , col) :
+                self.board [row][col] = 1
+                
+                if self.solve(row+1) :
+                    return True
+            
+                self.board[row][col] = 0 
+           
+        
+chessboard = Queens(4) 
+print(chessboard.solve())         
