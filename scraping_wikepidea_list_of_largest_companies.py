65c7f7a479d71e1911c043a012347c53fa846b65
Aboubakr Tahir <aboubakrtahir44@gmail.com>
2025-01-18 08:14:48 +0100

    Web Scraping a REAL Wikipedia: Largest U.S. Companies by Revenue
    A Python script using BeautifulSoup and pandas to scrape and parse the largest U.S. companies by revenue from Wikipedia, converting the data into a structured DataFrame.

---
+++ scraping_wikepidea_list_of_largest_companies.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests 
+
+page = requests.get("https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue")
+soup = BeautifulSoup(page.text , "html.parser")   
+
+table = soup.find_all('table')[0]
+columns_list = table.find_all('th')
+columns_table=[column.text.strip() for column in columns_list]
+
+
+
+
+import pandas as pd
+df = pd.DataFrame(columns = columns_table)
+
+row_tr = table.find_all('tr')
+for row in row_tr :
+  row_td = row.find_all('td')
+  each_row_list = [element.text.strip() for element in row_td]
+  
+  if each_row_list :
+    lenght = len(df)
+    df.loc[lenght] = each_row_list
+
+print(df)
